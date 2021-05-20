a3f0561c2634a1f482b3e05f8c7275e4ab2a6884
Lobo88 <47178940+Lobo88@users.noreply.github.com>
2021-05-20 13:51:46 +0200

    Add files via upload

---
+++ GameController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+namespace SubclassSandbox
+{
+    public class GameController : MonoBehaviour
+    {
+        
+        List<SuperPower> superPowers = new List<SuperPower>();
+
+        void Start()
+        {
+            
+            superPowers.Add(new SkyLaunch());
+            superPowers.Add(new GroundDive());
+        }
+
+        public bool NamePower(SuperPower power, string name)
+        {
+            
+            if (power.name.Equals("Dive"))
+            {
+                return true;
+            }
+            else return false; ;
+        }
+        public void findPower()
+        {
+
+            var item = superPowers.Find(x => x.name == "Dive");
+            // if (item == null)
+            //   throw new Exception();
+            Debug.Log(item+"lokalizacja");
+        }
+
+        void Update()
+        {
+
+            findPower();
+            for (int i = 0; i < superPowers.Count; i++)
+                {
+             
+                superPowers[i].Activate();
+                if (NamePower(superPowers[i], superPowers[i].name)==true) {
+                    Debug.Log("super power znaleziony");
+                };
+                }
+            
+        }
+    }
+}
---
+++ SuperPower.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace SubclassSandbox
+{
+   
+    public abstract class SuperPower
+    {
+        public string name { get; set; }
+        
+        
+        public abstract void Activate();
+
+       
+        protected void nameOfPower(string name)
+        {
+            Debug.Log("Name power " + name);
+            this.name = name;
+
+        }
+            protected void Move(float speed)
+        {
+            Debug.Log("Moving with speed " + speed);
+        }
+
+        protected void PlaySound(string coolSound)
+        {
+            Debug.Log("Playing sound " + coolSound);
+        }
+
+        protected void SpawnParticles()
+        {
+
+        }
+    }
+
+
+   
+    public class SkyLaunch : SuperPower
+    {
+        
+        public override void Activate()
+        {
+           
+            nameOfPower("jump");
+            Move(10f);
+            PlaySound("SkyLaunch");
+            SpawnParticles();
+        }
+    }
+
+    public class GroundDive : SuperPower
+    {
+       
+        public override void Activate()
+        {
+           
+            nameOfPower("Dive");
+            Move(15f);
+            PlaySound("GroundDive");
+            SpawnParticles();
+        }
+    }
+}
